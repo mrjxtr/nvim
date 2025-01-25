ba62df6d6e232b1e8d9bd7c19ad666577e62346c
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-01-25 08:44:24 +0800

    mappings to move lines up or down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,16 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- move lines using shift + j or k
+map("n", "<S-j>", ":m .+1<CR>==")
+map("n", "<S-k>", ":m .-2<CR>==")
+map("i", "<S-j>", "<Esc>:m .+1<CR>==gi")
+map("i", "<S-k>", "<Esc>:m .-2<CR>==gi")
+map("v", "<S-j>", ":m '>+1<CR>gv=gv")
+map("v", "<S-k>", ":m '<-2<CR>gv=gv")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
