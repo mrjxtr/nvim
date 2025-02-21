f79aa8b859744e8d9df6ab2e3178244bb0f7a6b6
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-02-21 19:56:52 +0800

    adding nvdash

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,6 +7,7 @@ local M = {}
 M.base46 = {
     theme = "ashes",
     -- theme = "material-darker",
+    -- transparency = true,
     -- hl_override = {
     --     Comment = { italic = true },
     --     ["@comment"] = { italic = true },
@@ -17,6 +18,46 @@ M.ui = {
     tabufline = {
         order = { "buffers", "tabs", "btns" },
         lazyload = false,
+    },
+}
+
+M.nvdash = {
+    load_on_startup = true,
+    header = {
+        "                            ",
+        "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+        "   ▄▀███▄     ▄██ █████▀    ",
+        "   ██▄▀███▄   ███           ",
+        "   ███  ▀███▄ ███           ",
+        "   ███    ▀██ ███           ",
+        "   ███      ▀ ███           ",
+        "   ▀██ █████▄▀█▀▄██████▄    ",
+        "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+        "                            ",
+        "     Powered By  eovim    ",
+        "                            ",
+    },
+
+    buttons = {
+        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+        },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
     },
 }
 
