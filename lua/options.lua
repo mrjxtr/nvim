af15b5b1b3eabdaa0dea8841ff7f64c306aa1da7
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-03-13 21:59:08 +0800

    update: use 2 character width for golang tabs

--- lua/options.lua
+++ lua/options.lua
@@ -26,11 +26,11 @@ vim.g.codeium_enabled = false
 
 -- Custom tabstop and shiftwidth for specific files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "lua", "python", "go" },
+    pattern = { "lua", "python" },
     command = "setlocal shiftwidth=4 tabstop=4",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "html", "css", "javascript", "typescript", "json", "jsonc" },
+    pattern = { "go", "html", "css", "javascript", "typescript", "json", "jsonc" },
     command = "setlocal shiftwidth=2 tabstop=2",
 })
