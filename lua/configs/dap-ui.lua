eb7ab5a9c2e9dce98d6c8d7620ee5e3a3566bbf0
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-03-26 17:07:36 +0800

    update: remove auto closing the dap ui

--- lua/configs/dap-ui.lua
+++ lua/configs/dap-ui.lua
@@ -3,7 +3,12 @@ local dapui = require("dapui")
 dapui.setup()
 
 local map = vim.keymap.set
-map("n", "<leader>du", "<cmd>lua require('dapui').toggle()<CR>", { desc = "Toggle DAP UI" })
+map(
+    "n",
+    "<leader>du",
+    "<cmd>lua require('dapui').toggle()<CR>",
+    { desc = "Toggle DAP UI" }
+)
 
 dap.listeners.after.attach.dapui_config = function()
     dapui.open()
@@ -11,9 +16,9 @@ end
 dap.listeners.after.launch.dapui_config = function()
     dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+-- dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+-- dapui.close()
+-- end
