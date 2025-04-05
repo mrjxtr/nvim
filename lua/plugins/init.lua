f5ef87fb3b732fc20a16afecdcbae98caa4590c9
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-04-05 11:04:57 +0800

    feat: nvim surround and makrdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -306,6 +306,29 @@ return {
         },
     },
 
+    -- Markdown Preview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
+    -- Surround selections
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end,
+    },
+
     -- edgy.nvim configuration
     {
         "folke/edgy.nvim",
