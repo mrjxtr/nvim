ad1028171531e2f3166bf4fe28e4d16709dcf12c
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-06-27 16:20:03 +0800

    updated: adde vue_ls to lspconfig.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -78,7 +78,7 @@ local options = {
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
-    --^lsp_format = "never",
+    -- lsp_format = "never",
     lsp_format = "fallback",
   },
 }
