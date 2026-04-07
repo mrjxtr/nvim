a41a95522f98ba75d5725a4c61799d7f054995ac
Jester Lumacad <mr.jesterlumacad@gmail.com>
2026-04-07 18:51:33 +0800

    update: lazy and mason plugins + removed unused variable

--- lua/configs/dap-python.lua
+++ lua/configs/dap-python.lua
@@ -1,7 +1,7 @@
 local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
 require("dap-python").setup(path)
 
-local map = vim.keymap.set
+-- local map = vim.keymap.set
 
 -- map("n", "<leader>dpr", function()
 --     require("dap-python").test_method()
